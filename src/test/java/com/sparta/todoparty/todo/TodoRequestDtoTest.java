d728a097a2bdd3b57e78e6a7af48e4322447eeb2
hyeon9won <mhw3901@gmail.com>
2023-12-04 08:40:44 +0900

    Todo Request DTO Test 추가

---
+++ src/test/java/com/sparta/todoparty/todo/TodoRequestDtoTest.java
@@ -0,0 +1,24 @@
+package com.sparta.todoparty.todo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoRequestDtoTest {
+
+    // 유효성 검사
+    @Test
+    public void noneTitleTest() {
+        // Given
+        TodoRequestDto todoRequestDto = new TodoRequestDto();
+        todoRequestDto.setTitle("제목 테스트");
+        todoRequestDto.setContent("내용 테스트");
+
+        // When
+        Todo todo = new Todo(todoRequestDto);
+
+        // Then
+        assertEquals(todoRequestDto.getTitle(), todo.getTitle());
+        assertEquals(todoRequestDto.getContent(), todo.getContent());
+    }
+}
