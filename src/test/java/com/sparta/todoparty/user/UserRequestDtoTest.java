1290b6bece1ed10cba4c66216b65e6f0e13e8523
hyeon9won <mhw3901@gmail.com>
2023-12-04 19:43:40 +0900

    fix: UserRequestDtoTest Displayname 추가 및 CommonTest 인터페이스 생성 후 TEST_USER_NAME(PASSWORD) 변수 사용

--- src/test/java/com/sparta/todoparty/user/UserRequestDtoTest.java
+++ src/test/java/com/sparta/todoparty/user/UserRequestDtoTest.java
@@ -1,34 +1,39 @@
 package com.sparta.todoparty.user;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.sparta.todoparty.CommonTest.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("유저 요청 DTO 생성")
 class UserRequestDtoTest {
 
+    @DisplayName("유저 요청 DTO 생성 성공")
     @Test
     void userRequestDtoTest() {
         // Given
         UserRequestDto userRequestDto = new UserRequestDto();
-        userRequestDto.setUsername("testuser");
-        userRequestDto.setPassword("Abcd1234");
+        userRequestDto.setUsername(TEST_USER_NAME);
+        userRequestDto.setPassword(TEST_USER_PASSWORD);
 
         // When
         String usernamePattern = "^[a-z0-9]{4,10}$";
         String passwordPattern = "^[a-zA-Z0-9]{8,15}$";
 
         // Then
-        assertTrue(userRequestDto.getUsername().matches(usernamePattern), "사용자명이 양식과 불일치");
-        assertTrue(userRequestDto.getPassword().matches(passwordPattern), "비밀번호이 양식과 불일치");
+        assertTrue(userRequestDto.getUsername().matches(usernamePattern));
+        assertTrue(userRequestDto.getPassword().matches(passwordPattern));
     }
 
+    @DisplayName("유저 요청 DTO 생성 실패 - 잘못된 username")
     @Test
     void usernameTest() {
         // Given
         UserRequestDto userRequestDto = new UserRequestDto();
 
-        userRequestDto.setUsername("tst"); // username이 4글자 보다 적게 입력 됐을 때
-        userRequestDto.setPassword("Abcd1234");
+        userRequestDto.setUsername("Invaild username"); // username이 잘못되었을 때
+        userRequestDto.setPassword(TEST_USER_PASSWORD);
 
         // When
         String usernamePattern = "^[a-z0-9]{4,10}$";
@@ -39,13 +44,14 @@ class UserRequestDtoTest {
         assertTrue(userRequestDto.getPassword().matches(passwordPattern), "잘못된 비밀번호 입력");
     }
 
+    @DisplayName("유저 요청 DTO 생성 실패 - 잘못된 password")
     @Test
     void passwordTest() {
         // Given
         UserRequestDto userRequestDto = new UserRequestDto();
-        userRequestDto.setUsername("testuser");
 
-        userRequestDto.setPassword("Abc123"); // password가 8글자 보다 적게 입력 됐을 때
+        userRequestDto.setUsername(TEST_USER_NAME);
+        userRequestDto.setPassword("Invaild password"); // password가 잘못되었을 때
 
         // When
         String usernamePattern = "^[a-z0-9]{4,10}$";
